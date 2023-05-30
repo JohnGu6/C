bbfdf130039a8c6e6dcd51bed5501186e1805b35
JohnGu6 <cscode1024@163.com>
2023-05-30 15:41:56 +0800

    Changes

---
+++ 01.chat类型转换double.c
@@ -0,0 +1,12 @@
+//#include <stdio.h>
+
+//chatÀàĞÍ×ª»»double
+//int main() {
+//    char c = 'A';
+//    double num = (double)c;
+//
+//    printf("Character: %c\n", c);
+//    printf("Integer value: %f\n", num);
+//
+//    return 0;
+//}
---
+++ main.c
@@ -0,0 +1,11 @@
+//#include <stdio.h>
+//
+//int main() {
+//    char c = 'A';
+//    double num = (double)c;
+//
+//    printf("Character: %c\n", c);
+//    printf("Integer value: %f\n", num);
+//
+//    return 0;
+//}
