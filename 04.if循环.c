f0b0240277a2e2cd1a7d7010fb3104401b816669
JohnGu6 <cscode1024@163.com>
2023-05-30 16:10:09 +0800

    Changes

--- 04.if循环.c
+++ 04.if循环.c
@@ -1,19 +1,19 @@
-#include <stdio.h>
-
-int main() {
-    int num = 15;
-
-    if (num > 10) {
-        printf("num 大于 10\n");
-
-        if (num > 20) {
-            printf("num 大于 20\n");
-        } else {
-            printf("num 小于等于 20\n");
-        }
-    } else {
-        printf("num 小于等于 10\n");
-    }
-
-    return 0;
-}
+//#include <stdio.h>
+//
+//int main() {
+//    int num = 15;
+//
+//    if (num > 10) {
+//        printf("num 大于 10\n");
+//
+//        if (num > 20) {
+//            printf("num 大于 20\n");
+//        } else {
+//            printf("num 小于等于 20\n");
+//        }
+//    } else {
+//        printf("num 小于等于 10\n");
+//    }
+//
+//    return 0;
+//}
